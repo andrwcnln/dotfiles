97ea28868662b23370440af81f37caf41cb62c1a
andrew <andrew@andrewconl.in>
2023-12-15 10:15:54 +0000

    [15/12/23] Adding fish config

---
+++ config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    set -U fish_greeting "🐟"
+
+    set -gx VISUAL nvim
+    set -gx EDITOR $VISUAL
+
+    fish_add_path /usr/local/go/bin
+    fish_add_path ~/go/bin
+end
+rvm default
+
+#function fish_prompt
+#    powerline-shell --shell bare $status
+#end
