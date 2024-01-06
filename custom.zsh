adb23257e269e02c7ae56af9efb855b60b72307c
Andrew Conlin <andrew@andrewconl.in>
2024-01-06 15:03:21 +0000

    [06/01/24] Adding `zvm` and some comments

--- custom.zsh
+++ custom.zsh
@@ -1,12 +1,29 @@
+# editor
 export VISUAL=nvim
 export EDITOR=$VISUAL
+
+# golang
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:~/go/bin
-export PATH=$PATH:~/.zig
+
+# zig
+# export PATH=$PATH:~/.zig
+
+# neovim
 export PATH=$PATH:/usr/bin/nvim-linux64/bin
+
+# picotool
+export PATH=$PATH:~/picotool/build
+
+# zig version manager
+export ZVM_INSTALL="$HOME/.zvm/self"
+export PATH="$PATH:$HOME/.zvm/bin"
+export PATH="$PATH:$ZVM_INSTALL/"
 
 # rvm default
 
 alias lg="lazygit"
 
-source /home/andrew/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+lolcat -F 0.5 ~/.zshbanner
+
+source /home/andrew/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
