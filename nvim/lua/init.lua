aa766faff156fc0d5a89c0f9c645f94d7f2da3ac
Andrew Conlin <andrew@andrewconl.in>
2024-08-01 16:29:20 +0100

    [2024-08-01] Add autocommand to reset cursor from nvim -> alacritty

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -21,3 +21,8 @@ vim.diagnostic.config({
         }
   })
 --vim.api.nvim_set_hl(0,"DiagnosticUnderlineWarn",{undercurl=true,sp='DiagnosticWarn'})
+vim.api.nvim_create_autocmd("ExitPre", {
+	group = vim.api.nvim_create_augroup("Exit", { clear = true }),
+	command = "set guicursor=a:ver90-blinkwait250-blinkoff250-blinkon250",
+	desc = "Set cursor back to beam when leaving Neovim."
+})
